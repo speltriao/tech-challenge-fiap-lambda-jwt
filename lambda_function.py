6e092028591f497aa59704ba3536d39ac4d834d6
Alexandre Speltri <alexandrespeltri@gmail.com>
2024-09-29 19:56:38 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,47 +1,41 @@
 import json
-import pg8000
-import os
 import jwt
+import datetime
 
 def lambda_handler(event, context):
-    token = event['headers'].get('Authorization')
-    token = remove_bearer_string(token)
-    method_arn = event['methodArn']
+    body = json.loads(event.get("body", "{}"))
+    cpf = body.get("cpf")
     
-    print(f"Authorization Token: {token}")
-    print(f"Method ARN: {method_arn}")
+    if not cpf or cpf == '':
+        cpf = os.environ['DEFAULT_CPF'] # O usuário escolheu não se autenticar
     
-    if token:
-        try:
-            cpf_from_token = get_cpf_from_jwt_token(token)
-            if is_valid_customer(cpf_from_token):
-                return generate_policy('user', 'Allow', method_arn)
-            return generate_policy('user', 'Deny', method_arn)
-        except Exception as e:
-            print(f"Error decoding token: {e}")
-            return generate_policy('user', 'Deny', method_arn)
-    else:
-        raise Exception('Unauthorized')
+    if not is_valid_customer(cpf):
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Usuário não cadastrado"})
+        }
 
-def get_cpf_from_jwt_token(token: str) -> str:
-    try:
-        payload = jwt.decode(token, os.environ['SECRET_KEY'], algorithms=['HS256'])
-        return payload.get('cpf')
-    except jwt.ExpiredSignatureError:
-        raise Exception("Token has expired")
-    except jwt.InvalidTokenError:
-        raise Exception("Invalid token")
+    secret = os.environ['SECRET']
+    payload = {
+        "cpf": cpf
+    }
+    
+    token = jwt.encode(payload, secret, algorithm="HS256")
 
-def remove_bearer_string(token: str) -> str:
-    return token.replace("Bearer ", "")
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "token": token
+        })
+    }
 
-def is_valid_customer(cpf_from_token: str) -> bool:
+def is_valid_customer(cpf: str) -> bool:
     db_host = os.environ['DB_HOST']
     db_name = os.environ['DB_NAME']
     db_user = os.environ['DB_USER']
     db_password = os.environ['DB_PASSWORD']
     
-    query = f"SELECT COUNT(cpf) FROM customer WHERE cpf = '{cpf_from_token}';"
+    query = f"SELECT COUNT(cpf) FROM customer WHERE cpf = '{cpf}';"
 
     try:
         connection = pg8000.connect(
@@ -62,23 +56,3 @@ def is_valid_customer(cpf_from_token: str) -> bool:
     except Exception as e:
         print(f"SQL execution failed: {e}")
         return False
-
-
-def generate_policy(principal_id, effect, resource) -> dict:
-    auth_response = {}
-    auth_response['principalId'] = principal_id
-    
-    if effect and resource:
-        policy_document = {}
-        policy_document['Version'] = '2012-10-17'
-        policy_document['Statement'] = []
-        
-        statement = {}
-        statement['Action'] = 'execute-api:Invoke'
-        statement['Effect'] = effect
-        statement['Resource'] = resource
-        
-        policy_document['Statement'].append(statement)
-        auth_response['policyDocument'] = policy_document
-    
-    return auth_response
